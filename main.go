19a8fd1df8bae74fe48c847139db1eccc265fffc
hizuheka <44028436+hizuheka@users.noreply.github.com>
2025-10-16 16:28:52 +0900

    rules.json対応

--- main.go
+++ main.go
@@ -8,29 +8,27 @@ import (
 
 // main関数は、サブコマンドのルーターとして機能します。
 func main() {
-	// サブコマンドが指定されているかチェック
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <command> [arguments]\n", os.Args[0])
 		fmt.Fprintf(os.Stderr, "Available commands: transform\n")
 		os.Exit(1)
 	}
 
-	// 最初の引数をサブコマンドとして解釈
 	subcommand := os.Args[1]
 
-	// サブコマンドに応じて処理を分岐
 	switch subcommand {
 	case "transform":
-		// transform コマンドの引数が正しいかチェック (program + transform + input + output = 4)
-		if len(os.Args) != 4 {
-			fmt.Fprintf(os.Stderr, "Usage: %s transform <input.xml> <output.xml>\n", os.Args[0])
+		// 引数の数をチェック (program + transform + rules + input + output = 5)
+		if len(os.Args) != 5 {
+			fmt.Fprintf(os.Stderr, "Usage: %s transform <rules.json> <input.xml> <output.xml>\n", os.Args[0])
 			os.Exit(1)
 		}
-		inputFilepath := os.Args[2]
-		outputFilepath := os.Args[3]
+		ruleFilepath := os.Args[2]
+		inputFilepath := os.Args[3]
+		outputFilepath := os.Args[4]
 
 		// XML変換処理を実行
-		if err := runTransform(inputFilepath, outputFilepath); err != nil {
+		if err := runTransform(ruleFilepath, inputFilepath, outputFilepath); err != nil {
 			log.Fatalf("Error during transform: %v", err)
 		}
 
